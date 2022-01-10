8424887fb98ac1d7d9d21117e33e0e1deaf13570
James <jamesg487@gmail.com>
2022-01-10 16:45:30 +0000

    added new python file containing my pets and pedros age

---
+++ precourse_recap.py
@@ -0,0 +1,15 @@
+dog = "pedro"
+
+cat = "mittens"
+
+pedros_age = 8
+
+def my_pets(pet1,pet2):
+    print("The name of my pets are %s and %s." % (pet1,pet2))
+
+def dog_years(age):
+    return age * 7
+
+my_pets(dog,cat)
+
+dog_years(pedros_age)
